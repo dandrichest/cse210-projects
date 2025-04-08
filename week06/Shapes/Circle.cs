79c011c2dd62e7bf76820b69eab10fdc0fd8e8e7
dandrichest <dandrichest@gmail.com>
2025-04-08 17:08:53 +0100

    shapes

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Shapes;
+
+public class Circle : Shape
+{
+    private double _radius = 0;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+
+
+}
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Drawing;
+
+namespace Shapes;
+
+public abstract class Shape
+{
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+
+    }
+
+    
+    // public virtual double GetArea()
+    // {
+    //     return 0;
+    // }
+    
+    public abstract double GetArea();
+
+}
